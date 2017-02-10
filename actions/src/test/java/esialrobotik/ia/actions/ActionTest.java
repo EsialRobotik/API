69335aadc05b4ac9eccd26dfd55687101d905ad8
François Prugniel <francois.prugniel@telecomnancy.net>
2017-02-10 23:35:53 +0100

    Création du projet sous forme de modules Maven 3

---
+++ actions/src/test/java/esialrobotik/ia/actions/ActionTest.java
@@ -0,0 +1,7 @@
+package esialrobotik.ia.actions;
+
+/**
+ * Created by franc on 10/02/2017.
+ */
+public class ActionTest {
+}
---
+++ detection/src/main/java/esialrobotik/ia/detection/DetectionI.java
@@ -0,0 +1,7 @@
+package esialrobotik.ia.detection;
+
+/**
+ * Created by franc on 10/02/2017.
+ */
+public interface DetectionI {
+}
