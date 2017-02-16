9efd96ada9812e0c15271fb9a292cdd5c6843814
François Prugniel <francois.prugniel@telecomnancy.net>
2017-02-17 00:06:22 +0100

    Classes de tests

---
+++ utils/src/test/java/esialrobotik/ia/utils/communication/raspberry/I2CTest.java
@@ -0,0 +1,7 @@
+package esialrobotik.ia.utils.communication.raspberry;
+
+/**
+ * Created by franc on 17/02/2017.
+ */
+public class I2CTest {
+}
--- utils/src/test/java/esialrobotik/ia/utils/gpio/PinTest.java
+++
@@ -1,7 +0,0 @@
-package esialrobotik.ia.utils.gpio;
-
-/**
- * Created by franc on 10/02/2017.
- */
-public class PinTest {
-}
