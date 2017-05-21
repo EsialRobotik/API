c08833ebe11486c5bfcc8b1403db73059da69771
Serrière Guillaume <guillaume.serriere@telecomnancy.net>
2017-05-21 20:34:31 +0200

    Add action module.
    Signed-off-by: Serrière Guillaume <guillaume.serriere@telecomnancy.net>

---
+++ actions/src/main/java/esialrobotik/ia/actions/ActionModule.java
@@ -0,0 +1,22 @@
+package esialrobotik.ia.actions;
+
+import com.google.inject.AbstractModule;
+import esialrobotik.ia.actions.a2017.Actions;
+
+/**
+ * Created by icule on 21/05/17.
+ */
+public class ActionModule extends AbstractModule{
+    private ActionModuleConfiguration actionModuleConfiguration;
+
+    public ActionModule(ActionModuleConfiguration actionModuleConfiguration) {
+        this.actionModuleConfiguration = actionModuleConfiguration;
+    }
+
+
+    @Override
+    protected void configure() {
+        bind(ActionModuleConfiguration.class).toInstance(actionModuleConfiguration);
+        bind(ActionInterface.class).to(Actions.class);
+    }
+}
